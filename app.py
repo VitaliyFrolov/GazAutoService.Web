23f5d399095c1b95418f0e04d74198adce040083
Vitaliy <vitaliy.frolov.dev@gmail.com>
2025-01-05 01:18:40 +0300

    feat(ci): create pipeline

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi import Request
 
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
