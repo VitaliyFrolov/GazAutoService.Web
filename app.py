59bffc23843af93973a7d3a1458a64c07234b4c8
Vitaliy <vitaliy.frolov.dev@gmail.com>
2025-01-06 13:41:56 +0300

    feat(templates): add html blocks

--- app.py
+++ app.py
@@ -14,10 +14,14 @@ templates = Jinja2Templates(directory="templates")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/info", response_class=HTMLResponse)
+@app.get("/about", response_class=HTMLResponse)
 async def read_info(request: Request):
-    return templates.TemplateResponse("info.html", {"request": request})
+    return templates.TemplateResponse("about.html", {"request": request})
 
 @app.post("/send")
 async def send(name: str = Form(...), email: str = Form(...), message: str = Form(...)):
     return f'Письмо от {name} успешно отправлено!'
+
+@app.get('/privacy')
+async def privacy():
+    return 'Privacy file'
