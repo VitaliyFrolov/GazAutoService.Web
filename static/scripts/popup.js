3298251b800c1559074d9076d08ea90fc98efd43
Vitaliy <vitaliy.frolov.dev@gmail.com>
2025-01-23 18:01:41 +0300

    feat(static/scripts/popup): add scroll blocker to opened popup

--- static/scripts/popup.js
+++ static/scripts/popup.js
@@ -5,15 +5,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
     openButton.addEventListener('click', () => {
         popupOverlay.style.display = 'flex';
+        document.body.classList.add('no-scroll');
     });
 
     closeButton.addEventListener('click', () => {
         popupOverlay.style.display = 'none';
+        document.body.classList.remove('no-scroll')
     });
 
     popupOverlay.addEventListener('click', (event) => {
         if (event.target === popupOverlay) {
             popupOverlay.style.display = 'none';
+            document.body.classList.remove('no-scroll');
         }
     });
 });
