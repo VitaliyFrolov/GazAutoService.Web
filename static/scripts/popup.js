b55c9e0c5166b40627dea69606673578b839b708
Vitaliy <vitaliy.frolov.dev@gmail.com>
2025-01-23 18:07:37 +0300

    feat(static/scripts/popup): add closed popup to esc key press

--- static/scripts/popup.js
+++ static/scripts/popup.js
@@ -3,20 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const closeButton = document.getElementById('closePopup');
     const popupOverlay = document.getElementById('popupOverlay');
 
+    const closePopup = () => {
+        popupOverlay.style.display = 'none';
+        document.body.classList.remove('no-scroll');
+        document.removeEventListener('keydown', handleEscapeKey);
+    };
+
+    const handleEscapeKey = (event) => {
+        if (event.key === 'Escape') {
+            closePopup();
+        }
+    };
+
     openButton.addEventListener('click', () => {
         popupOverlay.style.display = 'flex';
         document.body.classList.add('no-scroll');
+        document.addEventListener('keydown', handleEscapeKey);
     });
 
-    closeButton.addEventListener('click', () => {
-        popupOverlay.style.display = 'none';
-        document.body.classList.remove('no-scroll')
-    });
+    closeButton.addEventListener('click', closePopup);
 
     popupOverlay.addEventListener('click', (event) => {
         if (event.target === popupOverlay) {
-            popupOverlay.style.display = 'none';
-            document.body.classList.remove('no-scroll');
+            closePopup();
         }
     });
 });
