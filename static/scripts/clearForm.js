e4eda211ff0468d7ba57bd4a9f6188694ef1fe1e
Vitaliy <vitaliy.frolov.dev@gmail.com>
2025-03-05 19:02:56 +0300

    feat(index.html): add complited form modal

--- static/scripts/clearForm.js
+++ static/scripts/clearForm.js
@@ -1,6 +1,26 @@
 function clearForms() {
     setTimeout(() => {
         const forms = document.querySelectorAll('[data-form]');
+        const modal = document.querySelector('[data-completed-popup]');
+
         forms.forEach((form) => form.reset());
+
+        if (modal) {
+            modal.classList.add("show");
+
+            setTimeout(() => {
+                modal.classList.remove("show");
+            }, 2000);
+        }
     }, 500);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector('[data-form]');
+    if (form) {
+        form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            clearForms();
+        });
+    }
+});
